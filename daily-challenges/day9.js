78689c9f0a4f7e208dfec5c89a17702b6e0b1cb5
stefankarlsson <karlssonstefan88@gmail.com>
2017-12-09 13:11:38 +0100

    day9 done

--- daily-challenges/day9.js
+++ daily-challenges/day9.js
@@ -2,16 +2,39 @@
 
 let day9 = {
 
-	formatInput: function (inout) {
-
+	getAnswer1: function (input) {
+		return this.traverseInput(input);
 	},
 
-	getAnswer1: function (input) {
-		return input;
+	traverseInput: function (input, returnGarbage = false) {
+		let sum = 0;
+		let nestValue = 0;
+		let garbage = false;
+		let garbageSum = 0;
+
+		for (var i = 0; i < input.length; i++) {
+
+			if (input[i] === '<' && !garbage) {
+				garbage = true;
+			} else if (input[i] === '>') {
+				garbage =false;
+			} else if (input[i] === '{' && !garbage) {
+				nestValue++;
+			} else if (input[i] === '}' && !garbage) {
+				sum += nestValue;
+				nestValue--;
+			} else if (input[i] === '!') {
+				i++;
+			} else if (garbage) {
+				garbageSum++;
+			}
+		}
+
+		return returnGarbage ? garbageSum : sum;
 	},
 
 	getAnswer2: function (input) {
-		return input;
+		return this.traverseInput(input, true);
 	}
 }
 
--- tests/day9.test.js
+++ tests/day9.test.js
@@ -1,23 +1,69 @@
 const day9 = require('../daily-challenges/day9');
 const fs = require('fs');
 
-let testinput = fs.readFileSync(
-  'tests/test-inputs/day9.testinput',
-  {encoding: 'utf-8'});
-
-
 describe('getAnswer1', () => {
 
-  test('with testinput it should return 1', () => {
-    expect(day9.getAnswer1(testinput)).toBe(1);
+  test('input {} it should return 1', () => {
+    expect(day9.getAnswer1('{}')).toBe(1);
+  })
+
+  test('input {{{}}} it should return 6', () => {
+    expect(day9.getAnswer1('{{{}}}')).toBe(6);
+  })
+
+  test('input {{},{}} it should return 5', () => {
+    expect(day9.getAnswer1('{{},{}}')).toBe(5);
+  })
+
+  test('input {{{},{},{{}}}} it should return 16', () => {
+    expect(day9.getAnswer1('{{{},{},{{}}}}')).toBe(16);
+  })
+
+  test('input {<a>,<a>,<a>,<a>} it should return 1', () => {
+    expect(day9.getAnswer1('{<a>,<a>,<a>,<a>}')).toBe(1);
+  })
+
+  test('input {{<ab>},{<ab>},{<ab>},{<ab>}} it should return 9', () => {
+    expect(day9.getAnswer1('{{<ab>},{<ab>},{<ab>},{<ab>}}')).toBe(9);
+  })
+
+  test('input {{<!!>},{<!!>},{<!!>},{<!!>}} it should return 9', () => {
+    expect(day9.getAnswer1('{{<!!>},{<!!>},{<!!>},{<!!>}}')).toBe(9);
+  })
+
+  test('input {{<a!>},{<a!>},{<a!>},{<ab>}} it should return 3', () => {
+    expect(day9.getAnswer1('{{<a!>},{<a!>},{<a!>},{<ab>}}')).toBe(3);
+  })
+});
+
+describe('getAnswer2', () => {
+
+  test('input <> should return 0', () => {
+    expect(day9.getAnswer2('<>')).toBe(0);
+  })
+
+  test('input <random characters> should return 17', () => {
+    expect(day9.getAnswer2('<random characters>')).toBe(17);
+  })
+
+  test('input <<<<> should return 3', () => {
+    expect(day9.getAnswer2('<<<<>')).toBe(3);
+  })
+
+  test('input <{!>}> should return 2', () => {
+    expect(day9.getAnswer2('<{!>}>')).toBe(2);
+  })
+
+  test('input <!!> should return 0', () => {
+    expect(day9.getAnswer2('<!!>')).toBe(0);
+  })
+
+  test('input <!!!>> should return 0', () => {
+    expect(day9.getAnswer2('<!!!>>')).toBe(0);
+  })
+
+  test('input <{o"i!a,<{i<a> should return 10', () => {
+    expect(day9.getAnswer2('<{o"i!a,<{i<a>')).toBe(10);
   })
 
 });
-
-// describe('getAnswer2', () => {
-
-//   test('with testinput it should return 10', () => {
-//     expect(day9.getAnswer2(testinput)).toBe(10);
-//   })
-
-// });
